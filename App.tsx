0350e4353dd8ef5d6e274a2e67689f1c9c8a7ac2
Jesse Wachtel <84250138+blockchainTrucker@users.noreply.github.com>
2024-08-08 19:29:10 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,11 +2,11 @@
 import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNavigator from './navigation/TabNavigator';
-import {initGnus, processGnus} from './geniusSDK/src/geniusSDKWrapper';
+import {initGnus} from 'ts-wrapped-genius-sdk';
 
 export default function App() {
   useEffect(() => {
-    // initGnus();
+    initGnus();
     // processGnus('QmUDMvGQXbUKMsjmTzjf4ZuMx7tHx6Z4x8YH8RbwrgyGAf', 1);
   }, []);
   return (
